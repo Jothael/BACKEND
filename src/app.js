33a5c6e5e92586f6d2785cc4b5b2f3dc822e9746
Jothael <131524396+Jothael@users.noreply.github.com>
2023-12-30 16:44:34 -0300

    Websockets falta editar

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const handlebars = require('express-handlebars')
+const { Server: ServerIO, Socket } = require('socket.io')
+
 
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
 
+app.use(express.static(__dirname+'/public'))
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
+app.engine('handlebars', handlebars.engine())
+app.set('views',__dirname+'/views')
+app.set('view engine', 'handlebars')
+
+app.get('/', (req,res)=>{
+  res.render('home',{} )
+})
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+const httpServer = app.listen(8080, () => {
+  console.log(`Servidor corriendo en http://localhost:8080`);
 });
+
+const io = new ServerIO(httpServer)
+io.on('connection', socket =>{
+  console.log('cliente conectado')
+})
